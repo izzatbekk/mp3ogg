0276d5d3353900036a539ac071748550f7e4ee19
Izzatbek <58590849+izzatbekk@users.noreply.github.com>
2020-11-05 20:15:37 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -24,20 +24,28 @@ $message_id = $message->message_id;
 $chat_id = $message->chat->id;
 $user_id = $message->from->id;
 
+mkdir("data");
+
 if ($text == '/start') {
 bot('sendMessage',[
 'chat_id' => $chat_id,
-'text' => "mp3 yuboring",
+'text' => "Salom menga mp3 fayl yuboring men sizga 30-sekunndan boshlab 60-sekundgacha qirqaman va ovozli xabar ko'rinishida yuboraman",
 ]);
 }
 
 if ($message->audio) {
+bot('sendMessage',[
+'chat_id' => $chat_id,
+'text' => "Yuklanmoqda",
+]);
 $path = bot('getFile',['file_id'=>$message->audio->file_id])->result->file_path;
 $file = "https://api.telegram.org/file/bot$HTTP_API/$path";
-file_put_contents("$chat_id.mp3", file_get_contents($file));
-exec("ffmpeg -ss 30 -t 30 -i ".$chat_id.".mp3 -c:a libvorbis -b 8 ".$chat_id.".ogg");
+file_put_contents("data/$chat_id.mp3", file_get_contents($file));
+exec("ffmpeg -ss 30 -t 30 -i data/".$chat_id.".mp3 -c:a libvorbis -b 8 data/".$chat_id.".ogg");
 bot('sendVoice',[
 'chat_id'=>$chat_id,
-'voice'=>new CURLFile($chat_id.".ogg")
+'voice'=>new CURLFile("data/".$chat_id.".ogg")
 ]);
+unlink("data/$chat_id.mp3");
+unlink("data/$chat_id.ogg");
 }
