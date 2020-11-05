b7e87d6d5c4f06389d46508bc37ee2ae917c2736
Izzatbek <58590849+izzatbekk@users.noreply.github.com>
2020-11-05 15:02:12 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php
 
-if(exec('echo EXEC') == 'EXEC'){
-    echo 'exec works';
+$HTTP_API = '1466689222:AAHDHaMYkocO0csl2ynw2F6IgFqCsgdnLg4';
+
+function bot($method, $datas = []) {
+global $HTTP_API;
+$url = 'https://api.telegram.org/bot' . $HTTP_API . '/'. $method;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+$result = curl_exec($ch);
+if (curl_error($ch)) {
+var_dump(curl_error($ch));
+} else {
+return json_decode($result);
+}
+}
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$text = $message->text;
+$message_id = $message->message_id;
+$chat_id = $message->chat->id;
+$user_id = $message->from->id;
+
+if ($text == '/start') {
+bot('sendMessage',[
+'chat_id' => $chat_id,
+'text' => "ok true",
+]);
+}
+
+if ($message->audio) {
+$path = bot('getFile',['file_id'=>$message->audio->file_id])->result->file_path;
+$file = "https://api.telegram.org/file/bot$HTTP_API/$path";
+file_put_contents("audio.mp3", file_get_contents($file));
+exec("ffmpeg -ss 30 -t 30 -i audio.mp3 -acodec copy out.mp3");
+bot('sendaudio',[
+       'chat_id'=>$chat_id,
+       'audio'=>new CURLFile('out.mp3'),
+ 			]);
+unlink('out.mp3');
+unlink('audio.mp3');
 }
