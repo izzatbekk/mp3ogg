7e18145ec315cc5c01ec141a5ea1cdd58f4be78c
Izzatbek <58590849+izzatbekk@users.noreply.github.com>
2020-11-06 16:10:21 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ bot('sendMessage',[
 $path = bot('getFile',['file_id' =>$message->audio->file_id])->result->file_path;
 $file = "https://api.telegram.org/file/bot$HTTP_API/$path";
 file_put_contents("data/$chat_id.mp3", file_get_contents($file));
-exec("ffmpeg -ss 30 -t 30 -i data/".$chat_id.".mp3 -c:a libopus -b:a 16k data/".$chat_id.".ogg -y");
+exec("ffmpeg -ss 30 -t 30 -i data/".$chat_id.".mp3 -acodec libopus -b:a 16k data/".$chat_id.".ogg -y");
 bot('sendVoice',[
 'chat_id' => $chat_id,
 'voice' => new CURLFile("data/".$chat_id.".ogg"),
